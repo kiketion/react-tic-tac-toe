ec3442b6705472df050fa1bb341db2f08cb10c12
kiketion <kiketionje@gmail.com>
2022-04-04 18:12:37 +0300

    square created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import React from 'react';
+import SquareComponent from './SquareComponent';
 function App() {
   return (
     <div className='app-header'>
-      <h1>React Tic Tac Toe</h1>
+      <h1 className='heading-text'>React Tic Tac Toe</h1>
+      <div className='row jc-center'>
+        <SquareComponent />
+        <SquareComponent />
+        <SquareComponent />
+      </div>
+      <div className='row jc-center'>
+        <SquareComponent />
+        <SquareComponent />
+        <SquareComponent />
+      </div>
+      <div className='row jc-center'>
+        <SquareComponent />
+        <SquareComponent />
+        <SquareComponent />
+      </div>
     </div>
   );
 }
---
+++ src/SquareComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SquareComponent() {
+  return <span className='square'>X</span>;
+}
+
+export default SquareComponent;
